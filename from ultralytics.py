cc2d0e82f84c22f722f5a3d4cebf4339e499a433
unknown <refsam11@gmail.com>
2023-05-07 19:05:47 +0700

    edit label

--- from ultralytics.py
+++ from ultralytics.py
@@ -8,14 +8,15 @@ cam = cv2.VideoCapture(0)
 if not cam.isOpened():
     raise("No Camera")
 while True:
-    ret, image = cam.read()
+    ret, video = cam.read()
     if not ret: 
         break
     _time_mulai = time.time()
-    result = model.predict(image, show=True)
+    result = model.predict(video, show=True)
+    
 
     print("waktu", time.time()-_time_mulai) 
-    # cv2.imshow("image", image)
+    cv2.imshow("video_asli", video)
     _key = cv2.waitKey(1)
     if _key == ord('q'): 
         break
